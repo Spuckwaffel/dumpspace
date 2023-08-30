95a5a01cbcfe8c033e3b768281746dc593110c25
Spuckaffel <romangraber03@gmail.com>
2023-08-30 15:48:32 +0200

    init

---
+++ script.js
@@ -0,0 +1,22 @@
+// script.js
+
+document.getElementById("searchButton").addEventListener("click", function() {
+    alert("Button clicked!");
+    // You can add your own script logic here
+});
+
+
+const wordRow = document.getElementById('wordRow');
+
+fetch('items.json')
+    .then(response => response.json())
+    .then(data => {
+        const wordsArray = data.words;
+
+        wordsArray.forEach(word => {
+            const wordSpan = document.createElement('span');
+            wordSpan.textContent = word + ' ';
+            wordRow.appendChild(wordSpan);
+        });
+    })
+    .catch(error => console.error('Error fetching or parsing JSON:', error));
